1097bed19d74be3f15cb8fd27b8fb5d7eafdc8d2
Ridham Kakadiya <ridhamkakadiya2005@gmail.com>
2026-02-02 23:27:29 +0530

    code change

--- routes/product.js
+++ routes/product.js
@@ -1,15 +1,15 @@
 const express = require("express");
 const router = express.Router();
 
-const { addProduct, getAllProducts, deleteProduct, fetchProductForUpdate, updateData } = require("../controller/product");
+// const { addProduct, getAllProducts, deleteProduct, fetchProductForUpdate, updateData } = require("../controller/product");
 const roleMiddleware = require("../middleware/roleMiddleware");
 
 
-router.get("/", getAllProducts);
-router.post("/create", addProduct);
-router.post("/del/:id", roleMiddleware,  deleteProduct);
-router.put("/edit/:id", updateData);
-router.get("/edit/:id", fetchProductForUpdate);
+// router.get("/", getAllProducts);
+// router.post("/create", addProduct);
+// router.post("/del/:id", roleMiddleware,  deleteProduct);
+// router.put("/edit/:id", updateData);
+// router.get("/edit/:id", fetchProductForUpdate);
 
 
 module.exports = router;
